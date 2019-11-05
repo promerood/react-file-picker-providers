6442924eea24f10805e3710f792384724e6b18c7
Pablo Romero <pablo@ag2technology.com>
2019-11-05 11:33:14 -0300

    update

--- src/library/index.js
+++ src/library/index.js
@@ -1,10 +1,9 @@
 import DropboxChooser from "./dropbox/index";
-import GoogleChooser from "./google/index";
+// import GoogleChooser from "./google/index";
 import { PROVIDERS, EXTENSION_COMMON } from "./constants";
 
 export {
   DropboxChooser,
-  GoogleChooser,
   PROVIDERS,
   EXTENSION_COMMON
 };
