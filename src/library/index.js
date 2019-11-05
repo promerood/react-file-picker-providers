6fa9c936513c67e3cb4f38f9ac57fb098f19b164
Pablo Romero <pablo@ag2technology.com>
2019-11-05 12:00:17 -0300

    update babel

--- src/library/index.js
+++ src/library/index.js
@@ -1,9 +1,10 @@
 import DropboxChooser from "./dropbox/index";
-// import GoogleChooser from "./google/index";
+import GoogleChooser from "./google/index";
 import { PROVIDERS, EXTENSION_COMMON } from "./constants";
 
 export {
   DropboxChooser,
+  GoogleChooser,
   PROVIDERS,
   EXTENSION_COMMON
 };
